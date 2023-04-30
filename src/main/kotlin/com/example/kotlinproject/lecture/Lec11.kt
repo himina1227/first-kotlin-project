922096df71632c358d633816ec6c728165ab710c
himina1227 <himina1227@gmail.com>
2023-04-30 22:28:11 +0900

    - 코틀린에서 접근 제어를 다루는 방법

---
+++ src/main/kotlin/com/example/kotlinproject/lecture/Lec11.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinproject.lecture
+
+/**
+ * 코틀린에서 접근 제어를 다루는 방법
+ * 1. 자바와 코틀린의 가시성 제어
+ *  Java의 기본 접근 지시어는 default, Kotlin의 기본 접근 지시어는 public 이다
+ *  Kotlin에서 패키지는 namespace관리용이기 때문에 protected는 의미가 달라졌다.
+ *  Kotlin에서는 default가 사라지고, 모듈간의 접근을 통제하는 internal이 새로 생겼다.
+ * 2. 코틀린 파일의 접근 제어
+ * 3. 다양한 구성요소의 접근 제어
+ * 4. Java와 Kotlin을 함께 사용할 경우 주의할 점
+ */
+class Lec11 {
+}
