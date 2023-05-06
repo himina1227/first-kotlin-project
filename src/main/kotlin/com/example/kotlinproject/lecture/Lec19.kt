48c64a92171ba99377163af81bdf6efa408090d4
himina1227 <himina1227@gmail.com>
2023-05-06 21:09:12 +0900

    - 코틀린 이모저모

--- src/main/kotlin/com/example/kotlinproject/lecture/Lec19.kt
+++ src/main/kotlin/com/example/kotlinproject/lecture/Lec19.kt
@@ -1,4 +1,18 @@
 package com.example.kotlinproject.lecture
 
+/**
+ * 코틀린의 이모저모
+ * 1. Type Alias와 as Import
+ * 2. 구조분해와 componentN 함수
+ * 3. Jump와 Label
+ * 4. TakeIf와 TakeUnless
+ */
 class Lec19 {
+
 }
+
+data class UltraSuperGuardianTribe(
+    val name: String
+)
+
+typealias USGTMap = Map<String, UltraSuperGuardianTribe>
