b3799cac41f073ef9786f24527363635474ca57c
himina1227 <himina1227@gmail.com>
2023-05-06 21:34:35 +0900

    - 코틀린의 scope function

---
+++ src/main/kotlin/com/example/kotlinproject/lecture/Lec20.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinproject.lecture
+
+/**
+ * 코틀린의 scope function
+ * 1. scope fuction이란 무엇인가?
+ *  - 코틀린의 scope function 은 일시적인 영역을 만들어 코드를 더 간결하게 하거나, method chain에 활용된다.
+ * 2. scope function의 분류
+ *  - let/ run/ also/ apply/ with가 있다.
+ * 3. 언제 어떤 scope function을 사용해야 할까
+ * 4. scope function과 가독성
+ */
+class Lec20 {
+}
